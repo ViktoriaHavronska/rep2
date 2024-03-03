c3cb47090a277bd3bb5ab56132d18e7732e71f0a
ViktoriaHavronska <viktoriagavronska1@gmail.com>
2024-03-03 19:42:59 +0100

    modified problems in method showCalculator

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,6 @@ public class Calculator {
     int division(int num1, int num2 ) {
         return num1 / num2;
     }
-
     void showCalculator (){
         System.out.println("int sum (int num1, int num2);");
         System.out.println("int substract (int num1, int num2;");
