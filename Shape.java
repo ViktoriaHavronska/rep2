a5dfa60156890d6b8412997cdd19a3f089920a56
ViktoriaHavronska <viktoriagavronska1@gmail.com>
2024-03-03 14:07:43 +0100

    Shape

---
+++ Shape.java
@@ -0,0 +1,23 @@
+public class Shape {
+    void showTriangle(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    void showSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+
+    int calculateSquare(int num) {
+        return num * num;
+    }
+}
