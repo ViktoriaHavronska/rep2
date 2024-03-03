3e1741cac4e8a335bf68120643e9fc6a3eab9c6f
ViktoriaHavronska <viktoriagavronska1@gmail.com>
2024-03-03 19:20:43 +0100

    Calculator changed

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,14 @@
 public class Calculator {
     int sum (int num1, int num2 ){
-        System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
     return num1 + num2;
     }
-    int sub (int num1, int num2 ) {
-        System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+    int substract (int num1, int num2 ) {
         return num1 - num2;
     }
-    int mul (int num1, int num2 ) {
-        System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+    int multiply(int num1, int num2 ) {
         return num1 * num2;
     }
-    float div (float num1, float num2 ) {
-        System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
+    int division(int num1, int num2 ) {
         return num1 / num2;
     }
 
