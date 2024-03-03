6733a0bc01df89b4428a5976081e4bee3d50e3c5
ViktoriaHavronska <viktoriagavronska1@gmail.com>
2024-03-03 18:00:38 +0100

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+    int sum (int num1, int num2 ){
+        System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
+    return num1 + num2;
+    }
+    int sub (int num1, int num2 ) {
+        System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+        return num1 - num2;
+    }
+    int mul (int num1, int num2 ) {
+        System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+        return num1 * num2;
+    }
+    float div (float num1, float num2 ) {
+        System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
+        return num1 / num2;
+    }
+
+}
