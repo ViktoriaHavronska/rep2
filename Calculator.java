c3ee9b0a66da8b2b591fdc01d5ebb277c50f2280
ViktoriaHavronska <viktoriagavronska1@gmail.com>
2024-03-03 19:39:55 +0100

    Calculator changed 2

--- Calculator.java
+++ Calculator.java
@@ -12,11 +12,10 @@ public class Calculator {
         return num1 / num2;
     }
 
-    String showCalculator (){
-        System.out.println(" int sum (int num1, int num2 )");
-        System.out.println("int substract (int num1, int num2 )");
-        System.out.println(" int multiply(int num1, int num2 )");
-        System.out.println("int division(int num1, int num2 )");
-        return null;
+    void showCalculator (){
+        System.out.println("int sum (int num1, int num2);");
+        System.out.println("int substract (int num1, int num2;");
+        System.out.println("int multiply(int num1, int num2);");
+        System.out.println("int division(int num1, int num2);");
     }
 }
